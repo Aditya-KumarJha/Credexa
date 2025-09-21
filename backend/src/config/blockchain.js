ac1860cfaac57eeb018e43d425b51de997d104d8
rupampr <pradhanrupam07@gmail.com>
2025-09-21 11:32:30 +0530

    bugs fixed

--- backend/src/config/blockchain.js
+++ backend/src/config/blockchain.js
@@ -1,4 +1,4 @@
-const CredexaABI = require('../../../contracts/out/Credexa.sol/Credexa.json');
+const CredexaABI = require('./Credexa.json');
 
 const CONTRACT_ADDRESS = "0xd901248977dd6ea5fbdaea1e0f1caee3083f1234";
 
