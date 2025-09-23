3851b802f9459c3443a90a5411f07072123a40d4
adityajha <aj12102003@gmail.com>
2025-09-23 10:35:58 +0530

    Wallet glitch fixed

--- frontend/src/global.d.ts
+++ frontend/src/global.d.ts
@@ -1,7 +1,16 @@
-import { ethers } from "ethers";
+interface EthereumProvider {
+  isMetaMask?: boolean;
+  request: (request: { method: string; params?: Array<any> }) => Promise<any>;
+  on: (eventName: string, handler: (params?: any) => void) => void;
+  removeListener: (eventName: string, handler: (params?: any) => void) => void;
+  selectedAddress: string | undefined;
+  chainId: string | undefined;
+}
 
 declare global {
   interface Window {
-    ethereum?: ethers.providers.ExternalProvider;
+    ethereum?: EthereumProvider;
   }
 }
+
+export {};
